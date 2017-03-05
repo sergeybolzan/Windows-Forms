c7674f0b95d3074905a03889840f96ab92b79cdf
sergeybolzan <sergey.bolzan@gmail.com>
2017-03-05 10:32:42 +0300

    update

---
+++ MobileStore/MobileStore/Phone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileStore
+{
+    class Phone
+    {
+        public string Model { get; set; }
+        public string OperatingSystem { get; set; }
+        public double Processor { get; set; }
+        public double Price { get; set; }
+    }
+}
