78bccf836cad1839554027c6b6928e3089ac77bb
sergeybolzan <sergey.bolzan@gmail.com>
2017-03-17 20:47:29 +0300

    add DB

--- Autorization/Autorization/MainForm.cs
+++ Autorization/Autorization/MainForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,11 +16,41 @@ namespace Autorization
         public MainForm()
         {
             InitializeComponent();
-
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            SqlConnection connection = new SqlConnection();
+            try
+            {
+                SqlConnectionStringBuilder conString = new SqlConnectionStringBuilder();
+                conString.DataSource = @"(LocalDB)\v11.0";
+                conString.AttachDBFilename = Environment.CurrentDirectory + @"\registration.mdf";
+                conString.IntegratedSecurity = true;
+                connection.ConnectionString = conString.ConnectionString;
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Users", connection);
+                SqlDataReader reader = command.ExecuteReader();
+                string temp = null;
+                while (reader.Read() != false)
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        temp += reader[i] + " ";
+                    }
+                    temp += "\n";
+                }
+                reader.Close();
+                MessageBox.Show(temp);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
 
     }
