5f918e7cda3bd979359023d1e11e067009354c18
sergeybolzan <sergey.bolzan@gmail.com>
2017-04-27 23:47:56 +0300

    add myGMapMarker

--- BanksSearchApp/BanksSearchApp/MainForm.cs
+++ BanksSearchApp/BanksSearchApp/MainForm.cs
@@ -1,6 +1,7 @@
 ﻿using GMap.NET;
 using GMap.NET.WindowsForms;
 using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms.ToolTips;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -29,81 +30,52 @@ namespace BanksSearchApp
 
         private void LoadMap()
         {
-            // Создание элемента, отображающего карту !
             gMapControl = new GMapControl();
-            // Растягивание элемента на все окно!
             gMapControl.Dock = DockStyle.Fill;
-            // Добавление элемента 
             this.Controls.Add(gMapControl);
 
-            // ОБЩИЕ НАСТРОЙКИ КАРТЫ 
-            //Указываем, что будем использовать карты OpenStreetMap.
             gMapControl.MapProvider = GMap.NET.MapProviders.GMapProviders.OpenStreetMap;
-            // Указываем источник данных карты (выбран: интренети или локальный кэш)
             GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
 
-            // МАСШТАБИРОВАНИЕ
-            //Указываем значение максимального приближения.
             gMapControl.MaxZoom = 18;
-            //Указываем значение минимального приближения.
             gMapControl.MinZoom = 2;
-            //Указываем, что при загрузке карты будет использоваться 16ти кратной приближение.
-            gMapControl.Zoom = 16;
-            //Устанавливаем центр приближения/удаления курсор мыши.
+            gMapControl.Zoom = 17;
             gMapControl.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
 
             gMapControl.IgnoreMarkerOnMouseWheel = true;
-
-            // НАВИГАЦИЯ ПО КАРТЕ 
-            //CanDragMap - Если параметр установлен в True, пользователь может перетаскивать карту  помощью правой кнопки мыши. 
-            gMapControl.CanDragMap = true;
-
-            //Указываем что перетаскивание карты осуществляется с использованием левой клавишей мыши. По умолчанию - правая.
             gMapControl.DragButton = MouseButtons.Left;
-
-            //Указываем элементу управления, что необходимо при открытии карты перейти по координатам 
-            gMapControl.Position = new GMap.NET.PointLatLng(53.902800, 27.561759);
-
-
-            //ОТОБРАЖЕНИЕ МАРКЕРОВ НА КАРТЕ 
-            //MarkersEnabled - Если параметр установлен в True, любые маркеры, заданные вручную будет показаны.
+            gMapControl.Position = new PointLatLng(53.902800, 27.561759);
             gMapControl.MarkersEnabled = true;
 
-            //Создаем новый список маркеров, с указанием компонента в котором они будут использоваться и названием списка.
             GMapOverlay markersOverlay = new GMapOverlay("marker");
-            //Инициализация нового ЗЕЛЕНОГО маркера, с указанием его координат.
-            GMap.NET.WindowsForms.Markers.GMarkerGoogle markerG = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(new GMap.NET.PointLatLng(53.902542, 27.561781), GMarkerGoogleType.green);
-            markerG.Size = new System.Drawing.Size(3, 3);
-            markerG.ToolTip = new GMap.NET.WindowsForms.ToolTips.GMapRoundedToolTip(markerG);
-            //Текст отображаемый при наведении на маркер.
-            markerG.ToolTipText = "Объект №1";
-            markerG.ToolTipMode = MarkerTooltipMode.Always;
-
-
-
-
-
-            //Инициализация нового КРАСНОГО маркера, с указанием его координат.
-            GMap.NET.WindowsForms.Markers.GMarkerGoogle markerR = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(new GMap.NET.PointLatLng(53.902752, 27.561294), GMarkerGoogleType.red);
-            markerR.ToolTip = new GMap.NET.WindowsForms.ToolTips.GMapBaloonToolTip(markerR);
-            //Текст отображаемый при наведении на маркер.
-            markerR.ToolTipText = "Объект №2";
-
-            //Добавляем маркеры в список маркеров.
-            //Зеленый маркер
-            markersOverlay.Markers.Add(markerG);
-            //Красный маркет
-            markersOverlay.Markers.Add(markerR);
-            //Добавляем в компонент, список маркеров.
             gMapControl.Overlays.Add(markersOverlay);
 
-            // СОБЫТИЯ ПО КАРТЕ !
+
+
+            GMarkerGoogle markerG = new GMarkerGoogle(new PointLatLng(53.902542, 27.561781), GMarkerGoogleType.green);
+            markerG.ToolTip = new GMapRoundedToolTip(markerG);
+            markerG.ToolTipText = "Объект №1";
+            markerG.ToolTipMode = MarkerTooltipMode.Always;
+            markersOverlay.Markers.Add(markerG);
+
+            GMarkerGoogle markerR = new GMarkerGoogle(new PointLatLng(53.902752, 27.561294), GMarkerGoogleType.red);
+            markerR.ToolTip = new GMapBaloonToolTip(markerR);
+            markerR.ToolTipText = "Объект №2";
+            markersOverlay.Markers.Add(markerR);
+
+            GMarkerGoogle imageMarker = new GMarkerGoogle(new PointLatLng(53.902752, 27.561294), Properties.Resources.label);
+            markersOverlay.Markers.Add(imageMarker);
+
+            MyGMapMarkerImage imageMarker2 = new MyGMapMarkerImage(new PointLatLng(53.90227, 27.560604));
+            markersOverlay.Markers.Add(imageMarker2);
+            imageMarker2.ToolTip = new GMapToolTip(imageMarker2);
+            markerG.ToolTipMode = MarkerTooltipMode.OnMouseOver;
+            imageMarker2.ToolTipText = "asdasdadadada";
+            //imageMarker2.ToolTipPosition = imageMarker2.
+
+
             //gMapControl.MouseClick += gMapControl1_MouseClick;
-
         }
-
-
-
         //void gMapControl1_MouseClick(object sender, MouseEventArgs e)
         //{
         //    double X = gMapControl.FromLocalToLatLng(e.X, e.Y).Lng;
@@ -118,77 +90,116 @@ namespace BanksSearchApp
 
     }
 
-    public class GmapMarkerWithLabel : GMapMarker, ISerializable
+    public class MyGMapMarkerImage : GMarkerGoogle
     {
-        private Font font;
-        private GMarkerGoogle innerMarker;
-
-        public string Caption;
-
-        public GmapMarkerWithLabel(PointLatLng p, string caption, GMarkerGoogleType type)
-            : base(p)
+        public MyGMapMarkerImage(PointLatLng p)
+            : base(p, Properties.Resources.label)
         {
-            font = new Font("Arial", 14);
-            innerMarker = new GMarkerGoogle(p, type);
-
-            Caption = caption;
         }
 
         public override void OnRender(Graphics g)
         {
             base.OnRender(g);
-            g.DrawString(Caption, font, Brushes.Black, new PointF(LocalPosition.X - Size.Width / 2, LocalPosition.Y - Size.Height));
+            g.DrawString("2.885", new Font("Arial", 13), Brushes.Black, new PointF(LocalPosition.X + 10, LocalPosition.Y + 4));
         }
 
-        public override void Dispose()
+
+
+
+
+
+
+
+
+        public class GmapMarkerWithLabel : GMapMarker, ISerializable
         {
-            if (innerMarker != null)
+            private Font font;
+            private GMarkerGoogle innerMarker;
+
+            public string Caption;
+
+            public GmapMarkerWithLabel(PointLatLng p, string caption, GMarkerGoogleType type)
+                : base(p)
             {
-                innerMarker.Dispose();
-                innerMarker = null;
+                font = new Font("Arial", 14);
+                innerMarker = new GMarkerGoogle(p, type);
+
+                Caption = caption;
             }
 
-            base.Dispose();
+            public override void OnRender(Graphics g)
+            {
+                base.OnRender(g);
+                g.DrawString(Caption, font, Brushes.Black, new PointF(LocalPosition.X - Size.Width / 2, LocalPosition.Y - Size.Height));
+            }
+
+            public override void Dispose()
+            {
+                if (innerMarker != null)
+                {
+                    innerMarker.Dispose();
+                    innerMarker = null;
+                }
+
+                base.Dispose();
+            }
+
+            #region ISerializable Members
+
+            void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+            {
+                base.GetObjectData(info, context);
+            }
+
+            protected GmapMarkerWithLabel(SerializationInfo info, StreamingContext context)
+                : base(info, context)
+            {
+            }
+
+            #endregion
         }
 
-        #region ISerializable Members
 
-        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            base.GetObjectData(info, context);
-        }
 
-        protected GmapMarkerWithLabel(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-        }
 
-        #endregion
-    }
 
-    public class GMapMarkerRect : GMarkerGoogle
-    {
-        //public Pen Pen;
 
-        public GMapMarkerRect(PointLatLng p, GMarkerGoogleType type)
-            : base(p, type)
-        {
-            //Pen = new Pen(Brushes.Red, 5);
 
-            // do not forget set Size of the marker
-            // if so, you shall have no event on it ;}
-            //Size = new Size(55, 55);
-        }
 
-        public override void OnRender(Graphics g)
-        {
-            base.OnRender(g);
-            //g.DrawRectangle(Pen, new System.Drawing.Rectangle(LocalPosition.X - Size.Width / 2, LocalPosition.Y - Size.Height / 2, Size.Width, Size.Height));
-            var rect = new RectangleF(LocalPosition.X - Size.Width / 2 - 1, LocalPosition.Y - Size.Height / 2, 57, 17);
-            g.FillRectangle(Brushes.LightGreen, rect);
-            g.DrawString("2.885", new Font("Arial", 14), Brushes.Black, new PointF(LocalPosition.X - Size.Width / 2, LocalPosition.Y - Size.Height / 2 - 2));
 
-        }
+
+
+
+
+
+
+
+
+
+
+        //public class GMapMarkerRect : GMarkerGoogle
+        //{
+        //    //public Pen Pen;
+
+        //    public GMapMarkerRect(PointLatLng p, GMarkerGoogleType type)
+        //        : base(p, type)
+        //    {
+        //        //Pen = new Pen(Brushes.Red, 5);
+
+        //        // do not forget set Size of the marker
+        //        // if so, you shall have no event on it ;}
+        //        //Size = new Size(55, 55);
+        //    }
+
+        //    public override void OnRender(Graphics g)
+        //    {
+        //        base.OnRender(g);
+        //        //g.DrawRectangle(Pen, new System.Drawing.Rectangle(LocalPosition.X - Size.Width / 2, LocalPosition.Y - Size.Height / 2, Size.Width, Size.Height));
+        //        var rect = new RectangleF(LocalPosition.X - Size.Width / 2 - 1, LocalPosition.Y - Size.Height / 2, 57, 17);
+        //        g.FillRectangle(Brushes.LightGreen, rect);
+        //        g.DrawString("2.885", new Font("Arial", 14), Brushes.Black, new PointF(LocalPosition.X - Size.Width / 2, LocalPosition.Y - Size.Height / 2 - 2));
+
+        //    }
+        //}
     }
 }
-
